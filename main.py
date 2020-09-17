3c1e15d30168d19eca9b9d7e88e85a18568d19c4
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2020-09-17 20:10:44 +0700

    Second commit: removed some unused variables and unnecessary comment.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import random
 
 PATH = "C:\\Program Files (x86)\\chromedriver.exe"
-#driver = webdriver.Chrome(PATH)
 
 class hackernewsUpvoter():
     def __init__(self, username, password):
@@ -12,18 +12,28 @@ class hackernewsUpvoter():
         self.password = password
     
     def sign_in(self):
+
+        # Go to hackernews's website
         self.driver.get("https://news.ycombinator.com/login")
         time.sleep(2)
+
+        # Enter username  
         account = self.driver.find_element_by_name('acct')
-        account.send_keys("burgerrito")
+        account.send_keys(self.username)
+
+        # Enter password
         password = self.driver.find_element_by_name('pw')
-        password.send_keys("oboe*unexpired*footprint")
-        time.sleep(1)
+        password.send_keys(self.password)
+        time.sleep(random.randrange(11,35)/10)
+
+        # Click enter key
         password.send_keys(Keys.RETURN)
     
     def upvoter(self):
         self.driver.get("https://news.ycombinator.com/")
         upvoteButtons = self.driver.find_elements_by_class_name("votearrow")
+
+        # Click every upvote buttons in the page 
         for button in upvoteButtons:
             button.click()
             time.sleep(1)
@@ -33,7 +43,8 @@ class hackernewsUpvoter():
         more = self.driver.find_elements_by_class_name("morelink")
         more.click()
 
-bot = hackernewsUpvoter("burgerrito", "oboe*unexpired*footprint")
+bot = hackernewsUpvoter("username here", "password here")
+
 for i in range(3):
     bot.sign_in()
     bot.upvoter() 
@@ -41,18 +52,3 @@ for i in range(3):
 
 
 
-'''
-driver.get("https://news.ycombinator.com/login")
-print(driver.title)
-
-account = driver.find_element_by_name('acct')
-account.send_keys("burgerrito")
-password = driver.find_element_by_name('pw')
-password.send_keys("oboe*unexpired*footprint")
-password.send_keys(Keys.RETURN)
-
-print(driver.page_source)
-
-driver.quit()
-
-'''
